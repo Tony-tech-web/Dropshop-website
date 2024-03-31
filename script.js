5dbe4b7a62bd6c747f500078e3e4ecdd1ac75f2c
Tony-tech-web <146000675+Tony-tech-web@users.noreply.github.com>
2024-03-31 13:21:57 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,41 +1,28 @@
 document.addEventListener('DOMContentLoaded', function () {
+    const openBtn = document.getElementById('open-btn');
+    const closeBtn = document.getElementById('close-btn');
     const sidebar = document.getElementById('sidebar');
     const content = document.getElementById('content');
+
+    openBtn.addEventListener('click', function () {
+        sidebar.style.left = '0';
+        content.style.marginLeft = '250px';
+        document.cookie = "sidebarOpen=true";
+    });
+
+    closeBtn.addEventListener('click', function () {
+        sidebar.style.left = '-250px';
+        content.style.marginLeft = '0';
+        document.cookie = "sidebarOpen=false";
+    });
 
     // Check if sidebar was previously open
     const sidebarOpen = document.cookie.split(';').some((item) => item.trim().startsWith('sidebarOpen=true'));
     if (sidebarOpen) {
-        sidebar.style.width = '250px';
+        sidebar.style.left = '0';
         content.style.marginLeft = '250px';
     } else {
-        sidebar.style.width = '0px';
-        content.style.marginLeft = '0px';
+        sidebar.style.left = '-250px';
+        content.style.marginLeft = '0';
     }
-
-    // Close sidebar when clicking outside the sidebar area
-    window.addEventListener('click', function (event) {
-        if (event.target !== sidebar && !sidebar.contains(event.target)) {
-            sidebar.style.width = '0px';
-            content.style.marginLeft = '0px';
-            document.cookie = "sidebarOpen=false";
-        }
-    });
 });
-
-// Function to toggle menu and change its visibility
-function toggleMenu() {
-    const sidebar = document.getElementById('sidebar');
-    const content = document.getElementById('content');
-    const sidebarWidth = sidebar.style.width;
-
-    // Toggle sidebar visibility and adjust content margin accordingly
-    if (sidebarWidth === '0px' || !sidebarWidth) {
-        sidebar.style.width = '250px';
-        content.style.marginLeft = '250px';
-        document.cookie = "sidebarOpen=true";
-    } else {
-        sidebar.style.width = '0px';
-        content.style.marginLeft = '0px';
-        document.cookie = "sidebarOpen=false";
-    }
-}
